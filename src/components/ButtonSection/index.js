e1f5156cd8642bd828c470a95f7132007575a855
Ferguson Iyara <ferguson.iyara@redacreltd.com>
2022-12-04 22:07:17 +0100

    chore: make a change

--- src/components/ButtonSection/index.js
+++ src/components/ButtonSection/index.js
@@ -6,13 +6,13 @@ const ButtonSection = ({ handleCancel, handleSave }) => {
     <div className="flex items-center">
       <button
         onClick={handleCancel}
-        className="rounded-lg border-[1px] border-customGreen-100 py-2 px-5 text-customGreen-100 text-sm mr-6"
+        className="rounded-lg border-[1px] border-customGreen-100 py-2 px-3 text-customGreen-100 text-sm mr-6"
       >
         Cancel
       </button>
       <button
         onClick={handleSave}
-        className="flex rounded-lg bg-customGreen-100 py-2 px-5 text-sm items-center"
+        className="flex rounded-lg bg-customGreen-100 py-2 px-3 text-sm items-center"
       >
         <p className="text-white mr-3">Save</p>
         <img
