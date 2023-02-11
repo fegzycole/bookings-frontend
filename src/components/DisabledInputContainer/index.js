8fc4fcc77eac4cd24f8c532f48ffb0e12c73875e
Ferguson Iyara <ferguson.iyara@redacreltd.com>
2023-02-11 22:57:22 +0100

    chore: make a change

--- src/components/DisabledInputContainer/index.js
+++ src/components/DisabledInputContainer/index.js
@@ -7,9 +7,10 @@ const DisabledInputContainer = ({ label, value, fullwidth, bigText }) => {
       <p className="mb-2 font-Satoshi text-customBlack-900">{label}</p>
       {bigText ? (
         <textarea
-          value={value}
+          defaultValue={value}
           className="border-[1px] p-3 w-full mb-5 border-customGray-500 bg-white rounded-lg text-customBlack-800"
           rows={5}
+          disabled
         />
       ) : (
         <DisabledInput value={value} greyborder />
