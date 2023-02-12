eac2e00b68069e29bb432c3bf42f47c234a2b759
Ferguson Iyara <ferguson.iyara@redacreltd.com>
2023-02-12 16:19:17 +0100

    chore: make a change

--- src/containers/AdminPagesLayout/index.js
+++ src/containers/AdminPagesLayout/index.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import jwt from "jwt-decode";
 import IconButton from "@mui/material/IconButton";
@@ -48,8 +48,10 @@ const AdminPagesLayout = ({ children, helperText, title }) => {
   const accessToken = localStorage.getItem(ADMIN_ACCESS_TOKEN);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const location = useLocation();
   const [openModal, setOpenModal] = useState(false);
   const [showNav, setShowNav] = useState(false);
+  // const [selectedLink, setSelectedLink]
 
   const toggleModal = () => {
     setOpenModal((prev) => !prev);
@@ -78,6 +80,12 @@ const AdminPagesLayout = ({ children, helperText, title }) => {
     }
   }, [accessToken, navigate]);
 
+  // useEffect(() => {
+  //   if (location.pathname === BASE_ADMIN_URL) {
+  //     navigate(`${BASE_ADMIN_URL}/signin`);
+  //   }
+  // }, [navigate, location]);
+
   useInterval(() => {
     const decodedToken = jwt(accessToken);
     const currentDate = new Date();
@@ -104,56 +112,62 @@ const AdminPagesLayout = ({ children, helperText, title }) => {
           <Menu />
         </IconButton>
       </div>
-        <div
-          className={`bg-customYellow-300 min-h-[100vh] h-full w-[280px] p-5 absolute left-0 top-0 z-50 ${
-            showNav ? "block" : "hidden"
-          } lg:block`}
-        >
-          <div className="h-[90vh] flex flex-col justify-between">
+      <div
+        className={`bg-customYellow-300 min-h-[100vh] h-full w-[280px] p-5 absolute left-0 top-0 z-50 ${
+          showNav ? "block" : "hidden"
+        } lg:block`}
+      >
+        <div className="h-[90vh] flex flex-col justify-between">
+          <div>
+            <div className="flex flex-col relative z-40 text-center pb-10">
+              <img
+                src={ReginaPacisLogo}
+                alt="regina pacis logo"
+                className="w-[34px] lg:w-[70px] h-[34px] lg:h-[70px] mx-auto"
+              />
+              <h6 className="font-Museo text-base lg:text-base text-customGreen-100 font-medium">
+                Regina Pacis Catholic Church
+              </h6>
+              <h6 className="font-Museo text-base lg:text-xs text-customGreen-100 font-medium">
+                REGINA PACIS
+              </h6>
+            </div>
             <div>
-              <div className="flex flex-col relative z-40 text-center pb-10">
-                <img
-                  src={ReginaPacisLogo}
-                  alt="regina pacis logo"
-                  className="w-[34px] lg:w-[70px] h-[34px] lg:h-[70px] mx-auto"
-                />
-                <h6 className="font-Museo text-base lg:text-base text-customGreen-100 font-medium">
-                  Regina Pacis Catholic Church
-                </h6>
-                <h6 className="font-Museo text-base lg:text-xs text-customGreen-100 font-medium">
-                  REGINA PACIS
-                </h6>
-              </div>
-              <div>
-                {adminLinks.map((adminLink) => (
+              {adminLinks.map((adminLink) => {
+                const activePath = location.pathname === adminLink.to;
+                return (
                   <button
                     onClick={() => handleNavClick(adminLink.to)}
-                    className="flex items-start mb-10"
+                    className={`flex items-start mb-10 w-full ${
+                      activePath ? "border-l-[3px] border-customGreen-100" : ""
+                    }`}
+                    key={adminLink.to}
                   >
                     <img
                       src={adminLink.imgUrl}
                       alt={adminLink.text}
-                      className="mr-3 pt-[2px]"
+                      className={`${activePath ? "ml-3" : ""} mr-3 pt-[2px]`}
                     />
                     <h6 className="font-Museo text-base lg:text-base text-customGreen-100 font-medium">
                       {adminLink.text}
                     </h6>
                   </button>
-                ))}
-              </div>
-            </div>
-            <div>
-              <button onClick={toggleModal}>
-                <div className="flex">
-                  <img src={Logout} alt="Logout" className="mr-3" />
-                  <h6 className="font-Museo text-base lg:text-base text-customGreen-100 font-medium">
-                    Logout
-                  </h6>
-                </div>
-              </button>
+                );
+              })}
             </div>
           </div>
+          <div>
+            <button onClick={toggleModal}>
+              <div className="flex">
+                <img src={Logout} alt="Logout" className="mr-3" />
+                <h6 className="font-Museo text-base lg:text-base text-customGreen-100 font-medium">
+                  Logout
+                </h6>
+              </div>
+            </button>
+          </div>
         </div>
+      </div>
       <div className="w-full lg:w-[calc(100%_-_280px)] bg-customGray-300 p-5 font-Museo relative lg:ml-[280px] min-h-[100vh]">
         <header className="absolute bg-white top-0 left-0 w-full p-5">
           <h2 className="text-base lg:text-2xl text-customBlack-200">
