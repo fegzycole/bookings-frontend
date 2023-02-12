0211398081f24c05066c58b9d3935caeacb1ed8c
Ferguson Iyara <ferguson.iyara@redacreltd.com>
2023-02-12 16:00:57 +0100

    chore: make a style change

--- src/components/NoIntentions/index.js
+++ src/components/NoIntentions/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const NoIntentions = () => {
   return (
     <div className="w-full h-[300px] flex flex-col justify-center items-center">
-      <h3 className="text-5xl">No Intention Found</h3>
+      <h3 className="text:2xl md:text-5xl">No Intention Found</h3>
     </div>
   );
 };
