51e2b4d050bd632769ea9eab6596c7573f821968
Ferguson Iyara <ferguson.iyara@redacreltd.com>
2023-02-12 15:46:38 +0100

    chore: make a change

--- src/components/DisabledInputContainer/index.js
+++ src/components/DisabledInputContainer/index.js
@@ -3,7 +3,7 @@ import DisabledInput from "../DisabledInput";
 
 const DisabledInputContainer = ({ label, value, fullwidth, bigText }) => {
   return (
-    <div className={`${fullwidth ? "w-full" : "w-[45%]"}`}>
+    <div className={`${fullwidth ? "w-full" : "lg:w-[45%]"}`}>
       <p className="mb-2 font-Satoshi text-customBlack-900">{label}</p>
       {bigText ? (
         <textarea
